f0f7fb429f0c528210f2866a56b23a54ab316a4f
SupremeMortal <SupremeMortal@users.noreply.github.com>
2017-10-04 16:34:36 +0100

    Replace OpenSSL native hash with static mbed TLS for maximum compat.

--- natives/src/main/c/NativeHashImpl.cpp
+++ natives/src/main/c/NativeHashImpl.cpp
@@ -1,40 +1,44 @@
-#include <openssl/evp.h>
 #include <stdlib.h>
+
+#include <mbedtls/sha256.h>
 #include "com_voxelwind_server_jni_hash_NativeHashImpl.h"
 
 typedef unsigned char byte;
 
-void initializeDigest(EVP_MD_CTX *mdCtx) {
-    EVP_DigestInit_ex(mdCtx, EVP_sha256(), NULL);
+void initializeDigest(mbedtls_sha256_context *mdCtx) {
+	mbedtls_sha256_init(mdCtx);
+	mbedtls_sha256_starts(mdCtx, 0);
 }
 
 jlong JNICALL Java_com_voxelwind_server_jni_hash_NativeHashImpl_init(JNIEnv *env, jobject obj) {
-    EVP_MD_CTX *mdCtx = EVP_MD_CTX_create();
-    initializeDigest(mdCtx);
-    return (jlong) mdCtx;
+	mbedtls_sha256_context *mdCtx = (mbedtls_sha256_context*) malloc(sizeof (mbedtls_sha256_context));
+	initializeDigest(mdCtx);
+	return (jlong)mdCtx;
 }
 
 void JNICALL Java_com_voxelwind_server_jni_hash_NativeHashImpl_update(JNIEnv *env, jobject obj, jlong ctx, jlong in, jint bytes) {
-    EVP_DigestUpdate((EVP_MD_CTX*) ctx, (byte*) in, bytes);
+	mbedtls_sha256_context *mdCtx = (mbedtls_sha256_context*)ctx;
+	mbedtls_sha256_update(mdCtx, (byte*) in, bytes);
 }
 
 jbyteArray JNICALL Java_com_voxelwind_server_jni_hash_NativeHashImpl_digest(JNIEnv *env, jobject obj, jlong ctx) {
-    // SHA-256 produces 32 bytes (256 bits)
-    unsigned char output[32];
+	// SHA-256 produces 32 bytes (256 bits)
+	unsigned char output[32];
 
-    // Hash the final output.
-    EVP_MD_CTX *mdCtx = (EVP_MD_CTX*) ctx;
-    EVP_DigestFinal_ex(mdCtx, (unsigned char*) output, NULL);
+	// Hash the final output.
+	mbedtls_sha256_context *mdCtx = (mbedtls_sha256_context*)ctx;
+	mbedtls_sha256_finish(mdCtx, (unsigned char*) output);
 
-    // Reinitialize the context for further handling.
-    initializeDigest(mdCtx);
+	// Reinitialize the context for further handling.
+	initializeDigest(mdCtx);
 
-    // Copy array into Java byte array.
-    jbyteArray array = env->NewByteArray(32);
-    env->SetByteArrayRegion(array, 0, 32, (jbyte*) output);
-    return array;
+	// Copy array into Java byte array.
+	jbyteArray array = env->NewByteArray(32);
+	env->SetByteArrayRegion(array, 0, 32, (jbyte*)output);
+	return array;
 }
 
 JNIEXPORT void JNICALL Java_com_voxelwind_server_jni_hash_NativeHashImpl_free(JNIEnv *env, jobject obj, jlong ctx) {
-    EVP_MD_CTX_destroy((EVP_MD_CTX*) ctx);
+	mbedtls_sha256_context *mdCtx = (mbedtls_sha256_context*)ctx;
+	mbedtls_sha256_free(mdCtx);
 }
