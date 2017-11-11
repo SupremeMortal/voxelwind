7a0004397bfacaa2305276d946ce7ceb78d9ec4c
SupremeMortal <SupremeMortal@users.noreply.github.com>
2017-11-11 13:44:06 +0000

    Fix disconnect.

--- server/src/main/java/com/voxelwind/server/network/mcpe/packets/McpeDisconnect.java
+++ server/src/main/java/com/voxelwind/server/network/mcpe/packets/McpeDisconnect.java
@@ -2,14 +2,12 @@ package com.voxelwind.server.network.mcpe.packets;
 
 import com.voxelwind.server.network.NetworkPackage;
 import com.voxelwind.server.network.mcpe.McpeUtil;
-import com.voxelwind.server.network.mcpe.annotations.ForceClearText;
 import io.netty.buffer.ByteBuf;
 import lombok.Data;
 
 @Data
-@ForceClearText
 public class McpeDisconnect implements NetworkPackage {
-    private boolean hideDisconnectionScreen;
+    private boolean hideDisconnectionScreen = false;
     private String message;
 
     @Override
